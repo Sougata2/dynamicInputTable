1b08aeb5713a344cfa90639321678e017518f406
sougata <sougatadas@cdac.in>
2025-07-21 15:02:36 +0530

    eslint fixing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,19 +5,19 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended",
+    "plugin:react-hooks/recommended"
+    // ❌ REMOVE: "plugin:prettier/recommended"
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh", "prettier"],
+  plugins: ["react-refresh"],
   rules: {
     "react-hooks/exhaustive-deps": "warn",
-    "prettier/prettier": "warn",
+    // ❌ REMOVE: "prettier/prettier": ...
     "react-refresh/only-export-components": [
       "warn",
-      { allowConstantExport: true },
-    ],
-  },
+      { allowConstantExport: true }
+    ]
+  }
 };
